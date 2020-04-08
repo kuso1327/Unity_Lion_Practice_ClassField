0cbfb6b78eb9bf88158fda53f9253f0978c1ec1b
kuso1327 <63327433+kuso1327@users.noreply.github.com>
2020-04-08 16:32:37 +0800

    Unity_Lion_Practice_ClassField

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("血量"),Tooltip("怪物的血量")]
+    public int hp = 100;
+    public int mp = 100;
+    public float speed = 10.5f;
+    public int atk = 50;
+    public int def = 10;
+    public string weapon = "sword";
+    public string armor = "leather";
+    public bool key = false;
+    public bool treasure = false;
+    
+
+
+}
