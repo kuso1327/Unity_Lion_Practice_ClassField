b3ed734ec900f9cb35cb8f99dc0428153ac81b77
kuso1327 <63327433+kuso1327@users.noreply.github.com>
2020-04-09 16:02:55 +0800

    ClassField

--- ClassField/Assets/Enemy.cs
+++ ClassField/Assets/Enemy.cs
@@ -2,16 +2,50 @@
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
-    [Header("血量"),Tooltip("怪物的血量")]
-    public int hp = 100;
+{   
+    /// <summary>
+    /// 怪物血量
+    /// </summary>
+    [Header("血量"),Tooltip("怪物的血量"),Range(50,500)]
+    public int hp;
+    /// <summary>
+    /// 怪物魔力
+    /// </summary>
+    [Header("魔力"),Tooltip("怪物的魔力"),Range(10,100)]
     public int mp = 100;
+    /// <summary>
+    /// 怪物速度
+    /// </summary>
+    [Header("能力值"),Range(1,50.5F)]
     public float speed = 10.5f;
+    /// <summary>
+    /// 怪物攻擊力
+    /// </summary>
+    [Range(20,200)]
     public int atk = 50;
-    public int def = 10;
-    public string weapon = "sword";
+    /// <summary>
+    /// 怪物防禦力
+    /// </summary>
+    [Range(0,100)]
+    public int def;
+    /// <summary>
+    /// 怪物武器名稱
+    /// </summary>
+    [Header("裝備")]
+    public string weapon;
+    /// <summary>
+    /// 怪物裝備名稱
+    /// </summary>
     public string armor = "leather";
+    /// <summary>
+    /// 怪物是否帶鑰匙
+    /// </summary>
+    [Header("其他資料"),Tooltip("怪物是否帶有鑰匙")]
     public bool key = false;
+    /// <summary>
+    /// 怪物是否掉寶
+    /// </summary>
+    [Tooltip("怪物是否帶有寶物")]    
     public bool treasure = false;
     
 
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("怪物1號")]
+    public Enemy Monster1;
+
+    [Header("怪物2號")]
+    public Enemy Monster2;
+
+    private void Start()
+    {
+        //不小心多打了，就不刪了
+        print("怪物1的血量:"+Monster1.hp
+            + "怪物1的魔力:" + Monster1.mp
+            + "怪物1的移動速度:" + Monster1.speed
+            + "怪物1攻擊力:" + Monster1.atk
+            + "怪物1防禦力:" + Monster1.def
+            + "怪物1武器:" + Monster1.weapon
+            + "怪物1裝備:" + Monster1.armor
+            + "怪物1鑰匙:" + Monster1.key);
+        print("怪物2的血量:" + Monster2.hp
+            + "怪物2的魔力:" + Monster2.mp
+            + "怪物2的移動速度:" + Monster2.speed
+            + "怪物2攻擊力:" + Monster2.atk
+            + "怪物2防禦力:" + Monster2.def
+            + "怪物2武器:" + Monster2.weapon
+            + "怪物2裝備:" + Monster2.armor
+            + "怪物2鑰匙:" + Monster2.key);
+
+        Monster1.hp = 100;
+        Monster2.hp = 300;
+        Monster1.def = 50;
+        Monster1.weapon = "木棍";
+        Monster2.weapon = "短刀";
+        Monster1.key = true;
+        Monster2.treasure = true;
+
+
+    }
+}
